2dc2eea59c2055f75ecf9fa14b2fb736aff493c1
Giannis Tolios <derevirn@gmail.com>
2021-02-25 01:25:05 +0200

    Updated streamlit app

--- app.py
+++ app.py
@@ -1,57 +1,54 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
 import spacy
 from spacy.lang.el.stop_words import STOP_WORDS
 from wordcloud import WordCloud
 from utils import get_page_text
 
+st.set_page_config(page_title = "Greek Fake News Detector")
+
 @st.cache(allow_output_mutation=True)
 def get_nlp_model(path):   
     return spacy.load(path)
 
-def run_app(select_input):
-    if select_input == "URL":
-        url = st.text_input("URL")   
-        if st.button("Run"):
-            text = get_page_text(url)
-            
-            if nlp(text).cats['FAKE'] > nlp(text).cats['REAL']:
-                st.markdown("# <span style='color:red'>Fake news!</span>",
-                            unsafe_allow_html=True)
-            else:
-                st.markdown("## <span style='color:green'>Η είδηση είναι αληθής!<br>  \
-                            This news article is real!</span>",
-                            unsafe_allow_html=True)
-                    
-            st.markdown(text)        
-                
-            wc = WordCloud(width = 1000, height = 700,
-                           random_state = 1, background_color = 'white',
-                           stopwords = STOP_WORDS).generate(text) 
-            
-            fig, ax = plt.subplots()
-            ax.imshow(wc)
-            ax.axis('off')
-            st.pyplot(fig)
-            
-            print(nlp(text).cats)           
-    else:
-        text = st.text_area("Text", height=300)
-        if st.button("Run"):
-            print(nlp(text))
-            print(nlp(text).cats)
+def generate_output(text):
+     cats = nlp(text).cats
+     if cats['FAKE'] > cats['REAL']:
+         st.markdown("<h1><span style='color:red'>This is a fake news article!</span></h1>",
+                     unsafe_allow_html=True)
+     else:
+         st.markdown("<h1><span style='color:green'>This is a real news article!</span></h1>",
+                     unsafe_allow_html=True)
+             
+     q_text = '> '.join(text.splitlines(True))   
+     q_text = '> ' + q_text
+     st.markdown(q_text)
+
+     wc = WordCloud(width = 1000, height = 600,
+                    random_state = 1, background_color = 'white',
+                    stopwords = STOP_WORDS).generate(text) 
+     
+     fig, ax = plt.subplots()
+     ax.imshow(wc)
+     ax.axis('off')
+     st.pyplot(fig)
+     print(cats)
 
 nlp = get_nlp_model('model')
 
 st.title("Greek Fake News Detector")
-st.subheader("Είσαγετε τη Διεύθυνση URL ή το Κείμενο μίας Είδησης")
-
-
+st.subheader("Enter the URL address or the text of a greek news article.")
 select_input = st.radio("Select Input:", ["URL", "Text"])
 
 if select_input == "URL":
-    run_app(select_input)
-    
+    url = st.text_input("URL")   
+    if st.button("Run"):
+        text = get_page_text(url)
+        generate_output(text)  
+
 else:
-    run_app(select_input)
+    text = st.text_area("Text", height=250)
+    if st.button("Run"):
+        generate_output(text) 
 
